65aea3f22551fa1521a4ba314446943704d727c9
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-07-04 13:55:53 +0530

    Searchbar is completed

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,11 +1,53 @@
 "use client";
 
-import React from 'react'
+import { sracpeAndStoreProduct } from '@/lib/actions';
+import { FormEvent, useState } from 'react'
 
 const SearchBar = () => {
 
-  const handleSubmit = () => {
+  const [searchPrompt, setSearchPrompt] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
+  const isValidAmazonProductUrl = (url: string) => {
+    try {
+      const parsedUrl = new URL(url);
+      const hostName = parsedUrl.hostname;
+
+      // Check if hostname is amazon.com
+      if (
+        hostName.includes("amazon.com") ||
+        hostName.includes("amazon.") ||
+        hostName.endsWith("amazon")
+      ) {
+        return true;
+      }
+    } catch (error) {
+      console.log("Validation error: ", error);
+      return false;
+    }
+    return false;
+  };
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const isValidUrl = isValidAmazonProductUrl(searchPrompt);
+
+    if (!isValidUrl) {
+      alert('Invalid Amazon product URL');
+      return;
+    }
+
+    try {
+      setIsLoading(true);
+      
+      // Scrape the product details
+      const product = await sracpeAndStoreProduct(searchPrompt);
+    } catch (error) {
+      console.log("Error: ", error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -13,10 +55,20 @@ const SearchBar = () => {
       <input
         type="text"
         name=""
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
         placeholder='Enter product link'
         className='searchbar-input'
       />
-      <button type='submit' className='searchbar-btn'>Search</button>
+      <button
+        disabled={searchPrompt === '' || isLoading}
+        type='submit'
+        className='searchbar-btn'
+      >
+        {
+          isLoading ? 'Searching...' : 'Search'
+        }
+      </button>
     </form>
   )
 }
